d3c64ca2b7c54c2ff24188cce71b717a4b6c2a96
ghobot <greg@sciencelifeny.com>
2011-09-21 15:19:12 -0400

    functional with textmarks.com
    This works so far. On first message, you get instructions. When you
    play, you get the game status (you vs computer) and the result. Uses a
    result array.

--- palestine.php
+++ palestine.php
@@ -11,32 +11,45 @@ Instructions: Text P, I, or U for Palestine, Israel or USA to play a rock paper 
 
 http://sciencelifeny.com/itp2800/palestine.php?country=
 
-this is the practice url from textmarks.com:  http://you.com/script.php?from=\p&country=\0
+this is the practice url from textmarks.com:  http://you.com/script.php?from=\p&msg=\0
 
-http://sciencelifeny.com/itp2800/palestine.php?from=\p&country=\0
-
+http://sciencelifeny.com/itp2800/palestine.php?msg=\0
 */
 
 
+$player = $_GET['msg'];
 
 
-if ($_REQUEST != "i" || $_REQUEST != "p" || $_REQUEST != "u" )
-{echo "Reply to this msg with p (palestine), i (Israel), or u (USA) to play";}
-else {
+if ($player == "i" || $player == "p" || $player == "a")
+{
 
-country();
+$gameResult_array = country($player);
+
+echo $gameResult_array[0]." "."-";
+echo $gameResult_array[1];
+
+
+} else {
+
+
+echo "Reply to this msg with 'p' for Palestine, 'i' for Israel, or 'a' for USA to play.";
 
 }
 
 
 
-function country()
+function country($text)
 {
    
     // convert the text request to a numeric value  
-    if($_REQUEST['country'] == "p") $user_selection = 0;
-    if($_REQUEST['country'] == "i") $user_selection = 1;
-    if($_REQUEST['country'] == "u") $user_selection = 2;
+    if($text == "p") 
+    { $user_selection = 0;}
+    
+    if($text == "i") 
+    {$user_selection = 1;}
+    
+    if($text == "a") 
+    {$user_selection = 2;}
     
     // setup for the computers random play of U, I or P
     $logic[0] = 0;
@@ -46,38 +59,37 @@ function country()
     
     //if logic statements and outputs
     if($logic[$random_select] == $user_selection){
-        $game_message = "Truce.";
-        $game_status = "won";
+        $game_message = "Truce. Play again";
+        $game_status = "equal sides";
     }
     if($logic[$random_select] == 0 && $user_selection == 1){
         $game_message = "Israel launches an assault on Palestine. The international community isolates Israel - You win?";
-        $game_status = "won";
+        $game_status = "Israel vs Palestine";
     }
     if($logic[$random_select] == 2 && $user_selection == 1){
         $game_message = "The US stops giving Israel weapons. Israel must negotiate treaties with neighbors - You lose?";
-        $game_status = "lose";
+        $game_status = "Israel vs USA";
     }
     if($logic[$random_select] == 2 && $user_selection == 0){
         $game_message = "Palestine inspires a generation of islamist terrorists. Terrorists cripple the US economy - You win?";
-        $game_status = "won";
+        $game_status = "Palestine vs USA";
     }
     if($logic[$random_select] == 1 && $user_selection == 0){
-        $game_message = "Israel launches an assault on Palestine. Palenstine is leveled - You lose?";
-        $game_status = "lose";
+        $game_message = "Israel launches an assault on Palestine. Palestine is leveled - You lose?";
+        $game_status = "Palestine vs Israel";
     }
     if($logic[$random_select] == 1 && $user_selection == 2){
         $game_message = "The US stops giving Israel weapons. Israel negotiates a 2 state solution - You win?";
-        $game_status = "won";
+        $game_status = "USA vs Israel";
     }
     if($logic[$random_select] == 0 && $user_selection == 2){
         $game_message = "Palestine inspires a generation of islamist terrorists, making the US limit civil liberties - You lose?";
-        $game_status = "lose";
+        $game_status = "USA vs Palestine";
     }
-       
-	return echo {$game_message};
+    $result= array($game_status, $game_message);
+    
+    return $result;
 	
-	//echo "You:. {$country},. game:{$random_select} ,.{$game_message}";    
-	//return echo $country;
 }
 
 
