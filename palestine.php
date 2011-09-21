5d231399b0e5781079123c1d1705d02c196c559d
ghobot <greg@sciencelifeny.com>
2011-09-21 13:48:47 -0400

    further sms integration
    if statement for first time text from user, else, run the country
    function that compares the random play to the user play.

--- palestine.php
+++ palestine.php
@@ -18,24 +18,30 @@ http://sciencelifeny.com/itp2800/palestine.php?from=\p&country=\0
 */
 
 
-//$country = $_GET("country");
 
-echo "Please text Palestine (p), Israel (i), or USA (u) to start";
 
-/*
-function country_plugin_callback()
+if ($_REQUEST != "i" || $_REQUEST != "p" || $_REQUEST != "u" )
+{echo "Reply to this msg with p (palestine), i (Israel), or u (USA) to play";}
+else {
+
+country();
+
+}
+
+
+
+function country()
 {
-    
-       
-	// convert the text request to a numeric value  
-    if($_REQUEST['country'] == "palestine" || $_REQUEST['country'] == "p" ) $user_selection = 0;
-    if($_REQUEST['country'] == "usa" || $_REQUEST['country'] == "u") $user_selection = 1;
-    if($_REQUEST['country'] == "israel" || $_REQUEST['country'] == "i") $user_selection = 2;
+   
+    // convert the text request to a numeric value  
+    if($_REQUEST['country'] == "p") $user_selection = 0;
+    if($_REQUEST['country'] == "i") $user_selection = 1;
+    if($_REQUEST['country'] == "u") $user_selection = 2;
     
     // setup for the computers random play of U, I or P
-    $logic[0] = "palestine";
-    $logic[1] = "usa";
-    $logic[2] = "israel";
+    $logic[0] = 0;
+    $logic[1] = 1;
+    $logic[2] = 2;
     $random_select = rand(0,2);
     
     //if logic statements and outputs
@@ -43,35 +49,36 @@ function country_plugin_callback()
         $game_message = "Truce.";
         $game_status = "won";
     }
-    if($logic[$random_select] == "palestine" && $_REQUEST['country'] == "israel"){
+    if($logic[$random_select] == 0 && $user_selection == 1){
         $game_message = "Israel launches an assault on Palestine. The international community isolates Israel - You win?";
         $game_status = "won";
     }
-    if($logic[$random_select] == "usa" && $_REQUEST['country'] == "israel"){
+    if($logic[$random_select] == 2 && $user_selection == 1){
         $game_message = "The US stops giving Israel weapons. Israel must negotiate treaties with neighbors - You lose?";
         $game_status = "lose";
     }
-    if($logic[$random_select] == "usa" && $_REQUEST['country'] == "palestine"){
+    if($logic[$random_select] == 2 && $user_selection == 0){
         $game_message = "Palestine inspires a generation of islamist terrorists. Terrorists cripple the US economy - You win?";
         $game_status = "won";
     }
-    if($logic[$random_select] == "israel" && $_REQUEST['country'] == "palestine"){
+    if($logic[$random_select] == 1 && $user_selection == 0){
         $game_message = "Israel launches an assault on Palestine. Palenstine is leveled - You lose?";
         $game_status = "lose";
     }
-    if($logic[$random_select] == "israel" && $_REQUEST['country'] == "usa"){
+    if($logic[$random_select] == 1 && $user_selection == 2){
         $game_message = "The US stops giving Israel weapons. Israel negotiates a 2 state solution - You win?";
         $game_status = "won";
     }
-    if($logic[$random_select] == "palestine" && $_REQUEST['country'] == "usa"){
+    if($logic[$random_select] == 0 && $user_selection == 2){
         $game_message = "Palestine inspires a generation of islamist terrorists, making the US limit civil liberties - You lose?";
         $game_status = "lose";
     }
        
-	return echo "You:. {$country},. game:{$random_select} ,.{$game_message}";    
+	return echo {$game_message};
+	
+	//echo "You:. {$country},. game:{$random_select} ,.{$game_message}";    
 	//return echo $country;
 }
-*/
 
 
 ?>
